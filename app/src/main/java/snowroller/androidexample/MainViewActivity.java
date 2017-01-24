2d90b95e3cb7ea37ac1a4e51c3f0ed659fedac4a
Martin Blomberg <martin.blomberg@outlook.com>
2017-01-24 15:20:17 +0100

    Föreläsning 4
    MainViewActivity added with constraintslayout example.

---
+++ app/src/main/java/snowroller/androidexample/MainViewActivity.java
@@ -0,0 +1,13 @@
+package snowroller.androidexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view);
+    }
+}
---
+++ app/src/main/java/snowroller/androidexample/Notes.java
@@ -0,0 +1,37 @@
+package snowroller.androidexample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import snowroller.androidexample.databinding.ActivityNotesBinding;
+
+public class Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_notes);
+        ActivityNotesBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_notes);
+        binding.setMyDataBindingHelper(MyDataBindingHelper.get("Bound"));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+}
